0a5247e81cdbcccba1c421fbca491917e5a2ef6a
qusifan <qusifan@gaosiedu.com>
2018-03-21 14:20:05 +0800

    add all

--- src/main/java/sf/niotomcat/SampleController.java
+++ src/main/java/sf/niotomcat/SampleController.java
@@ -1,9 +1,19 @@
 package sf.niotomcat;
 
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
-import org.springframework.web.bind.annotation.*;
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.Http11NioProtocol;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.Servlet;
 
 @Controller
 @EnableAutoConfiguration
@@ -14,6 +24,48 @@ public class SampleController {
     String home() {
         return "Hello World!";
     }
+
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+        tomcat.setPort(8064);
+        tomcat.addAdditionalTomcatConnectors(createNioConnector());
+
+        return tomcat;
+    }
+
+    @Bean
+    public Servlet getDispatcherServlet(){
+        return new DispatcherServlet();
+    }
+
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean(){
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(getDispatcherServlet(),"/**");
+        servletRegistrationBean.setAsyncSupported(true);
+        return servletRegistrationBean;
+    }
+
+
+    private ThreadLocal<String> s = new ThreadLocal<>();
+
+    private Connector createNioConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(38065);
+        connector.setAsyncTimeout(100000);
+        connector.setEnableLookups(false);
+        connector.setRedirectPort(8443);
+        connector.setURIEncoding("UTF-8");
+        Http11NioProtocol protocol = (Http11NioProtocol) connector.getProtocolHandler();
+        protocol.setAcceptorThreadCount(1);
+        protocol.setAcceptCount(5);
+        protocol.setMaxConnections(5);
+        protocol.setProcessorCache(1024);
+        protocol.setMaxThreads(1);
+        return connector;
+    }
+
+
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(SampleController.class, args);
---
+++ src/main/java/sf/niotomcat/tomcat/TomcatConfig.java
@@ -0,0 +1,15 @@
+package sf.niotomcat.tomcat;
+
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+
+/**
+ * Created by Administrator on 2018/3/20.
+ */
+public class TomcatConfig implements EmbeddedServletContainerCustomizer {
+
+    @Override
+    public void customize(ConfigurableEmbeddedServletContainer configurableEmbeddedServletContainer) {
+        configurableEmbeddedServletContainer.setPort(6666);
+    }
+}
